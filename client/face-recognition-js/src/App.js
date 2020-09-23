1404a183fd210ecacf9a9312c044cb48e14edeb3
Daniel <danisan9490@gmail.com>
2020-09-23 11:22:03 +0100

    #10 Add Particules JS Background

--- client/face-recognition-js/src/App.js
+++ client/face-recognition-js/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
 
 function App() {
+
+  const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 350
+        }
+      }
+    }
+  }
+
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
