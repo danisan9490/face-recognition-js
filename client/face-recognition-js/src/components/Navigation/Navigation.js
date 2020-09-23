4fcf40b419c9fb68862d15e8ac303c5540fb43fd
Daniel <danisan9490@gmail.com>
2020-09-23 09:31:21 +0100

    #5 Delete CSS  Navigation Component

--- client/face-recognition-js/src/components/Navigation/Navigation.js
+++ client/face-recognition-js/src/components/Navigation/Navigation.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import './Navigation.css';
 
 function Navigation() {
   return (
     <div className='Navigation'>
       <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        <p className='f3 link dim black underline pa3'>Sign Out</p>
       </nav>
     </div >
   );
