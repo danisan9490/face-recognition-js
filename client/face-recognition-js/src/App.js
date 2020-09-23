266dc7ab4824ffe621a076de78d1be53fe23538a
Daniel <danisan9490@gmail.com>
2020-09-23 10:52:47 +0100

    #9 Create Rank Component

--- client/face-recognition-js/src/App.js
+++ client/face-recognition-js/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+
       {/* <FaceRecognition /> */}
     </div>
   );
---
+++ client/face-recognition-js/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Rank() {
+  return (
+    <div>
+      <div className="white f3">
+        {'Dani your current Rank is....'}
+      </div>
+      <div className="white f1">
+        {'#5'}
+      </div>
+    </div >
+  );
+}
+
+export default Rank;
